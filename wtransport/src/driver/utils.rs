b831b51593fc9413149824daa0bbea280196fbcf
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2023-06-20 20:58:03 +0200

    driver/utils: add UTs and internal documentation

--- wtransport/src/driver/utils.rs
+++ wtransport/src/driver/utils.rs
@@ -43,6 +43,12 @@ where
         Self(Arc::new(watch::channel(None).0))
     }
 
+    /// Sets the shared result in thread safe manner.
+    ///
+    /// The first call will be able to actuall set the inner value,
+    /// successive calles end up into being no-op.
+    ///
+    /// Returns `true` if the inner result is actually set.
     pub fn set(&self, result: T) -> bool {
         self.0.send_if_modified(|state| {
             if state.is_none() {
@@ -54,10 +60,15 @@ where
         })
     }
 
+    /// Awaits all subscribers are dead.
     pub async fn closed(&self) {
         self.0.closed().await
     }
 
+    /// Generates a new subscriber.
+    ///
+    /// A subscriber is able to be notified when the shared result
+    /// will be set.
     pub fn subscribe(&self) -> SharedResultGet<T> {
         SharedResultGet(Mutex::new(self.0.subscribe()))
     }
@@ -69,6 +80,13 @@ impl<T> SharedResultGet<T>
 where
     T: Copy,
 {
+    /// Awaits the shared result is set by any setter.
+    ///
+    /// Once the shared result is set, this method will always
+    /// return that value (i.e., `Some(T)`).
+    ///
+    /// If all setters are dead before setting any result, this will
+    /// return `None`. And all successive calls will return `None`.
     pub async fn result(&self) -> Option<T> {
         let mut lock = self.0.lock().await;
 
@@ -79,6 +97,75 @@ where
 
             if lock.changed().await.is_err() {
                 return None;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use utils::poll_once;
+
+    #[tokio::test]
+    async fn shared_result_double_set() {
+        let set = SharedResultSet::new();
+        assert!(set.set(1));
+        assert!(!set.set(2));
+
+        let get = set.subscribe();
+        assert!(matches!(get.result().await, Some(1)));
+    }
+
+    #[tokio::test]
+    async fn shared_result_get_drop() {
+        let set = SharedResultSet::<()>::new();
+        let get = set.subscribe();
+        drop(set);
+        assert!(get.result().await.is_none());
+        assert!(get.result().await.is_none());
+    }
+
+    #[tokio::test]
+    async fn shared_result_get() {
+        let set = SharedResultSet::new();
+        let get = set.subscribe();
+
+        assert!(matches!(poll_once(get.result()).await, None));
+
+        set.set(1);
+        drop(set);
+
+        assert!(matches!(poll_once(get.result()).await.unwrap(), Some(1)));
+        assert!(matches!(poll_once(get.result()).await.unwrap(), Some(1)));
+    }
+
+    mod utils {
+        use std::future::Future;
+        use std::pin::Pin;
+        use std::task::Context;
+        use std::task::Poll;
+
+        pub async fn poll_once<F, T>(future: F) -> Option<T>
+        where
+            F: Future<Output = T>,
+        {
+            PollOnce(Box::pin(future)).await
+        }
+
+        struct PollOnce<F>(Pin<Box<F>>);
+
+        impl<F, T> Future for PollOnce<F>
+        where
+            F: Future<Output = T>,
+        {
+            type Output = Option<T>;
+
+            fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+                match Future::poll(self.0.as_mut(), cx) {
+                    Poll::Ready(result) => Poll::Ready(Some(result)),
+                    Poll::Pending => Poll::Ready(None),
+                }
             }
         }
     }
