2fd6d1bf627429f633492bd970efbcf80e1d00f2
Josh Triplett <josh@joshtriplett.org>
2025-10-13 22:41:42 -0700

    Enable `unused_qualifications` warning and fix all instances
    This ensures, for instance, that if a module imports `std::fmt` and then
    writes `std::fmt::Formatter`, it gets a warning with an automatic fix to
    change it to `fmt::formatter`.

--- wtransport-proto/src/varint.rs
+++ wtransport-proto/src/varint.rs
@@ -123,14 +123,14 @@ impl From<VarInt> for u64 {
 
 impl fmt::Debug for VarInt {
     #[inline]
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.0.fmt(f)
     }
 }
 
 impl fmt::Display for VarInt {
     #[inline]
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.0.fmt(f)
     }
 }
